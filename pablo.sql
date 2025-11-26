7a72b27aacc82b92c369430db739d3780098b0b5
Pablo Herrero <pablohgb@gmail.com>
2025-11-26 14:16:52 +0100

    añadido sql de pablo

---
+++ pablo.sql
@@ -0,0 +1,72 @@
+INSERT INTO idiomas (idioma, nivel, equipo_id)
+VALUES
+('Español', 'Nativo', 2),
+('Inglés', 'Avanzado', 2);
+
+INSERT INTO proyectos (nombreProyecto, descripcion, linkRepo, tecnologias, equipo_id, orden)
+VALUES
+('NEBRIGAME', 'Página e-commerce de compra-venta de videojuegos, merchandising y periféricos de ordenador.', NULL, 'React, Node.js', 2, 1),
+('APOLLOFY MUSIC PROJECT', 'Reproductor de música con un diseño estilo Spotify.', 'https://github.com/pablohgb/Apollofy-music-app', 'React, TypeScript, Tailwind, Node.js, MongoDB', 2, 2),
+('TRAVEL-PLANNER', 'Gestión de la información de tus próximos viajes.', 'https://github.com/pablohgb/travel-planner-back', 'React, JavaScript, Node.js, Express.js, MongoDB', 2, 3),
+('FILESYSTEM EXPLORER', 'Aplicación similar al explorador de archivos en Windows/macOS.', 'https://github.com/pablohgb/filesystem-explorer', 'PHP, Bootstrap', 2, 4);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('HTML', 'Avanzado', 'Frontend', 1, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('CSS', 'Avanzado', 'Frontend', 2, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('PHP', 'Intermedio', 'Backend', 3, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('JavaScript', 'Avanzado', 'Frontend', 4, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('TypeScript', 'Básico', 'Frontend', 5, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('React', 'Avanzado', 'Frontend', 6, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('npm', 'Avanzado', 'DevOps', 7, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Node.js', 'Avanzado', 'Backend', 8, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Express.js', 'Avanzado', 'Backend', 9, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Microsoft Azure', 'Básico', 'Otro', 10, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Java', 'Intermedio', 'Backend', 11, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('C', 'Básico', 'Backend', 12, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Bash scripts', 'Avanzado', 'DevOps', 13, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Python', 'Básico', 'Backend', 14, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Windows Server', 'Básico', 'DevOps', 16, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Linux (comandos avanzados)', 'Avanzado', 'DevOps', 17, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Microsoft Office', 'Intermedio', 'Otro', 18, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Salesforce CRM', 'Básico', 'Otro', 19, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('Asana', 'Intermedio', 'Otro', 20, 2);
+
+INSERT INTO tecnologias (nombre, nivel, categoria, orden, equipo_id)
+VALUES ('ClickUp', 'Intermedio', 'Otro', 21, 2);
+
