ef0a93ee40382e7173a51e95153f1862737ce4cb
Sofia <sofialacrodriguez@outlook.com>
2025-12-10 10:05:54 +0100

    borrado de comentarios

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ app.engine("handlebars", exphbs.engine());
 
 app.set("view engine", "handlebars");
 
-// Middleware para archivos estáticos
 app.use(express.static("html_plantilla"));
 
-//Indico que si voy a home, me cargue home
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-// Usar las rutas definidas en rutas.js (después de la ruta home para evitar conflictos)
 app.use("/", rutas);
